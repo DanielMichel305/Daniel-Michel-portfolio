03520c31c202143b478ef9ad5823aab30a50a597
DanielMichel305 <daniel2202556@miuegypt.edu.eg>
2025-06-17 01:12:21 +0300

    Some Comments 3shan aftkr b3ml eh

--- routers/CmsRouter.ts
+++ routers/CmsRouter.ts
@@ -4,6 +4,17 @@ import { CmsController } from '../Controllers/CMSController';
 export const CmsRouter = Router();
 
 
+/*
+- Auth Middleware using env vars for this proj.
+- Work on the blog Dashboard
+- Fix DB migrations
+- Move root level routes to /blogs
+- Modify Navbar buttons to be blog, portfolio, contact, etc..and add actual links 
+- Start working on Main Page
+- Deploy
+*/
+
+
 CmsRouter.get('/', (req: Request, res: Response)=>{res.send("LALAL")})
 CmsRouter.post('/upload', CmsController.UploadMedia)
 
